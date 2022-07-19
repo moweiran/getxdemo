01483697a7236cf96cdf4aedcd1693d185f52522
lzy <iamlzy@163.com>
2022-07-19 16:33:50 +0800

    travis

--- lib/pages/custom_painters/custom_painter_demo.dart
+++ lib/pages/custom_painters/custom_painter_demo.dart
@@ -11,11 +11,57 @@ class _CustomPainterDeomState extends State<CustomPainterDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 100,
-        height: 100,
-        child: Text('CustomPainterDemo'),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 200,
+          ),
+          ClipPath(
+            clipper: MessageClipper(borderRadius: 1),
+            child: Container(
+              height: 200,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(1)),
+                color: Colors.red,
+              ),
+              child: const Center(
+                child: Text('MessageClipper'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+/// [MessageClipper], can be used with [ClipPath] widget, and clips the widget in dialog box shape
+class MessageClipper extends CustomClipper<Path> {
+  MessageClipper({this.borderRadius = 15});
+  final double borderRadius;
+  @override
+  Path getClip(Size size) {
+    double width = size.width;
+    double height = size.height;
+    double rheight = height - height / 3;
+    double oneThird = width / 3;
+
+    final path = Path()
+      ..lineTo(0, rheight - borderRadius)
+      ..cubicTo(0, rheight - borderRadius, 0, rheight, borderRadius, rheight)
+      ..lineTo(oneThird, rheight)
+      ..lineTo(width / 2 - borderRadius, height - borderRadius)
+      ..cubicTo(width / 2 - borderRadius, height - borderRadius, width / 2,
+          height, width / 2 + borderRadius, height - borderRadius)
+      ..lineTo(2 * oneThird, rheight)
+      ..lineTo(width - borderRadius, rheight)
+      ..cubicTo(width - borderRadius, rheight, width, rheight, width,
+          rheight - borderRadius)
+      ..lineTo(width, 0)
+      ..lineTo(0, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
